dd3b620b4297f2cb9620a1c91cbbb0e5f305cd0a
Michael Krog <mic@apaq.dk>
2020-05-29 15:23:28 +0200

    Vendor spring

--- src/main/java/com/previsto/dinero/repository/Resource.java
+++ src/main/java/com/previsto/dinero/repository/Resource.java
@@ -39,7 +39,7 @@ public abstract class Resource<T extends Persistable<String>> {
     }
 
     public List<T> findAll() {
-        return findAll(new PageRequest(0, 1000)).getContent();
+        return findAll(PageRequest.of(0, 1000)).getContent();
     }
 
     public Page<T> findAll(PageRequest pageRequest) {
